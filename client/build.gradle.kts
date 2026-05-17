a31d7fff57ae980e89cb5e724f8c01a15290afd4
SmakoszJan <margos.michal@gmail.com>
2026-05-17 16:26:58 +0200

    Scaffolding for a client

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:15.0")
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- client/src/main/java/org/tcs/Main.java
+++ client/src/main/java/org/tcs/Main.java
@@ -4,20 +4,34 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
 
 public class Main extends Application {
   @Override
-  public void start(Stage primaryStage) {
+  public void start(@NotNull Stage primaryStage) {
     primaryStage.setTitle("Chess Manager");
 
-    var root = new Label("Hello, World!");
+    var tabs = List.of("Tournaments", "Players", "Clubs", "Arbiters");
 
-    Scene scene = new Scene(root, 800, 600);
+    var pane = new TabPane();
+
+    for (var tab : tabs) {
+      pane.getTabs().add(new Tab(tab, new Label(tab)));
+    }
+
+    Scene scene = new Scene(pane, 800, 600);
     primaryStage.setScene(scene);
     primaryStage.show();
   }
 
+  // IDEA marks this as unused, even though it's used.
+  @SuppressWarnings("unused")
   static void main() {
     Application.launch();
   }
