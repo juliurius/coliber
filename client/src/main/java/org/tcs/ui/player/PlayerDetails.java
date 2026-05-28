f8acb7b4829d7a0ec8770480bf9156925fc3444d
SmakoszJan <margos.michal@gmail.com>
2026-05-28 20:42:51 +0200

    Penalties and norms are now displayed in player details

--- client/src/main/java/org/tcs/ui/player/PlayerDetails.java
+++ client/src/main/java/org/tcs/ui/player/PlayerDetails.java
@@ -1,13 +1,16 @@
 package org.tcs.ui.player;
 
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import org.tcs.Globals;
+import org.tcs.backend.Backend;
+import org.tcs.backend.Norm;
+import org.tcs.backend.Penalty;
 import org.tcs.backend.Player;
 import org.tcs.ui.Nav;
 import org.tcs.ui.Util;
@@ -20,7 +23,7 @@ public class PlayerDetails extends VBox {
 
   private final ObjectProperty<Consumer<Nav>> onNav = new SimpleObjectProperty<>(_ -> {});
 
-  public PlayerDetails() {
+  public PlayerDetails(Backend backend) {
     var button = new Button("Back");
     getChildren().add(button);
     button.setOnAction(_ -> onNav.get().accept(new Nav.Player(null)));
@@ -96,6 +99,56 @@ public class PlayerDetails extends VBox {
     clubLink.disableProperty().bind(player.map(v -> v.club() == null));
     clubLabel.setLabelFor(clubLink);
     getChildren().add(Util.inline(clubLabel, clubLink));
+
+    var penaltiesLabel = new Label("Penalties");
+    var penalties = new TableView<Penalty>();
+
+    var untilColumn = new TableColumn<Penalty, String>("Until");
+    untilColumn.setCellValueFactory(
+        p -> new SimpleStringProperty(p.getValue().until().toLocalDate().toString()));
+    penalties.getColumns().add(untilColumn);
+
+    var reasonColumn = new TableColumn<Penalty, String>("Reason");
+    reasonColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().reason()));
+    penalties.getColumns().add(reasonColumn);
+
+    var arbiterColumn = new TableColumn<Penalty, String>("Arbiter");
+    arbiterColumn.setCellValueFactory(
+        p -> new SimpleStringProperty(p.getValue().arbiter().toString()));
+    penalties.getColumns().add(arbiterColumn);
+
+    player.addListener(
+        _ ->
+            backend
+                .getPlayerPenalties(player.get().id())
+                .thenAccept(v -> Platform.runLater(() -> penalties.getItems().setAll(v))));
+
+    getChildren().addAll(penaltiesLabel, penalties);
+
+    var normsLabel = new Label("Penalties");
+    var norms = new TableView<Norm>();
+
+    var titleColumn = new TableColumn<Norm, String>("Title");
+    titleColumn.setCellValueFactory(
+      p -> Bindings.createStringBinding(() -> {
+        if (globals.get() == null) return "";
+        return globals.get().title(p.getValue().title()).name();
+      }, globals));
+    titleColumn.setMinWidth(200);
+    norms.getColumns().add(titleColumn);
+
+    var tournamentColumn = new TableColumn<Norm, String>("Tournament");
+    tournamentColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().tournament().name()));
+    tournamentColumn.setMinWidth(400);
+    norms.getColumns().add(tournamentColumn);
+
+    player.addListener(
+      _ ->
+        backend
+          .getPlayerNorms(player.get().id())
+          .thenAccept(v -> Platform.runLater(() -> norms.getItems().setAll(v))));
+
+    getChildren().addAll(normsLabel, norms);
   }
 
   public ObjectProperty<Player> playerProperty() {
