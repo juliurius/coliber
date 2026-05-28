b8f558deb29eb2c24e3b1ff88c94187218a667d5
SmakoszJan <margos.michal@gmail.com>
2026-05-28 22:24:32 +0200

    Game definitions in backend

---
+++ client/src/main/java/org/tcs/backend/Game.java
@@ -0,0 +1,8 @@
+package org.tcs.backend;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public record Game(@NotNull PlayerBrief white, @NotNull PlayerBrief black, @Nullable Over over) {
+  public record Over(boolean whiteWon, @NotNull GameOverReason reason, @NotNull PlayerBrief arbiter) {}
+}
