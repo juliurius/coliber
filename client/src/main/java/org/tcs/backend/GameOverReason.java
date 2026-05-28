a04b6defe39df7618a1a67991bad4fec2aca0390
SmakoszJan <margos.michal@gmail.com>
2026-05-28 21:22:05 +0200

    Update game over reasons to new db

--- client/src/main/java/org/tcs/backend/GameOverReason.java
+++ client/src/main/java/org/tcs/backend/GameOverReason.java
@@ -2,6 +2,6 @@ package org.tcs.backend;
 
 import org.jetbrains.annotations.NotNull;
 
-public record GameOverReason(@NotNull Id id, @NotNull String description) {
+public record GameOverReason(@NotNull Id id, @NotNull String description, float winScore, float loseScore) {
   public interface Id {}
 }
