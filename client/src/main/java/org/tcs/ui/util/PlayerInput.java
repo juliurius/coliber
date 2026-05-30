1904819a0b697f7ae1a36c8efb0ecfd6e61edd6a
SmakoszJan <margos.michal@gmail.com>
2026-05-30 23:50:06 +0200

    Player and tournament inputs can now be searched

--- client/src/main/java/org/tcs/ui/util/PlayerInput.java
+++ client/src/main/java/org/tcs/ui/util/PlayerInput.java
@@ -2,24 +2,37 @@ package org.tcs.ui.util;
 
 import javafx.application.Platform;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import org.tcs.backend.Backend;
 import org.tcs.backend.Player;
 import org.tcs.backend.PlayerBrief;
+import org.tcs.ui.Util;
 
 public class PlayerInput extends VBox {
   private final SimpleObjectProperty<Player.Id> player = new SimpleObjectProperty<>();
 
   public PlayerInput(Backend backend) {
     var label = new Label("Player");
+    var search = new TextField();
+    var prompt = new SimpleStringProperty("");
+    search.promptTextProperty().bind(prompt);
     var list = new ListView<Entry>();
     label.setLabelFor(list);
     var items = FXCollections.<Entry>observableArrayList();
-    list.setItems(items);
+    var filtered = items.filtered(_ -> true);
+    filtered
+        .predicateProperty()
+        .bind(
+            search
+                .textProperty()
+                .map(s -> e -> e.getText().toLowerCase().contains(s.toLowerCase())));
+    list.setItems(filtered);
 
     player.bind(list.getSelectionModel().selectedItemProperty().map(v -> v.id));
 
@@ -28,12 +41,12 @@ public class PlayerInput extends VBox {
     clear.setOnAction(_ -> list.getSelectionModel().clearSelection());
 
     backend
-      .getPlayers()
-      .thenAccept(
-        players ->
-          Platform.runLater(() -> items.setAll(players.stream().map(Entry::new).toList())));
+        .getPlayers()
+        .thenAccept(
+            players ->
+                Platform.runLater(() -> items.setAll(players.stream().map(Entry::new).toList())));
 
-    getChildren().addAll(label, list);
+    getChildren().addAll(Util.inline(label, search), list);
   }
 
   public Player.Id getValue() {
