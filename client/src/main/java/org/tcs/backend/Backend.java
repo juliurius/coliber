c784d24477fb58c372f8a1f3f1c607d45f8cacc9
piotr <piotr.rainer613@gmail.com>
2026-05-30 19:14:26 +0200

    torunament

--- client/src/main/java/org/tcs/backend/Backend.java
+++ client/src/main/java/org/tcs/backend/Backend.java
@@ -47,4 +47,13 @@ public interface Backend {
   CompletableFuture<String> createTempo(Tempo.Data data);
   CompletableFuture<String> createTournamentSystem(TournamentSystem.Data data);
   CompletableFuture<String> createClub(Club.Data data);
+
+  CompletableFuture<String> addTournamentPlayer(Tournament.Id tournament, Player.Id player);
+  CompletableFuture<String> createRound(Tournament.Id tournament, Timestamp start, Timestamp end);
+  CompletableFuture<String> createGame(Round.Id round, Player.Id white, Player.Id black);
+  CompletableFuture<String> setGameResult(
+          Round.Id round, Player.Id white, boolean whiteWon,
+          GameOverReason.Id reason, Player.Id arbiter,
+          int whiteRatingChange, int blackRatingChange);
+  CompletableFuture<String> closeTournament(Tournament.Id tournament);
 }
